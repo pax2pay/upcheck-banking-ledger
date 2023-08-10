1b6693bd57736b39465b231c7a294b292e5abff9
Emilia By <emilia.by@pax2pay.com>
2023-08-10 11:33:17 +0200

    Correct type in case of internal.

--- index.spec.ts
+++ index.spec.ts
@@ -56,20 +56,25 @@ describe("pax2pay Ledger", () => {
 		).toBeTruthy()
 	})
 	it("create paxgiro", async () => {
+		let type: pax2pay.Rail.Type = "paxgiro"
 		!accounts
 			? undefined
 			: (accounts?.[0]?.balances?.USD?.actual ?? 0) > (accounts?.[1]?.balances?.USD?.actual ?? 0)
 			? ((sourcePaxgiro = accounts?.[0].id),
 			  (targetPaxgiro =
-					accounts?.[1].rails[0].type == "paxgiro" ? accounts?.[1].rails[0].identifier : accounts?.[1].id))
+					accounts?.[1].rails[0].type == "paxgiro"
+						? accounts?.[1].rails[0].identifier
+						: (accounts?.[1].id, (type = "internal"))))
 			: ((sourcePaxgiro = accounts?.[1].id),
 			  (targetPaxgiro =
-					accounts?.[0].rails[0].type == "paxgiro" ? accounts?.[0].rails[0].identifier : accounts?.[0].id))
+					accounts?.[0].rails[0].type == "paxgiro"
+						? accounts?.[0].rails[0].identifier
+						: (accounts?.[0].id, (type = "internal"))))
 		const transaction: pax2pay.Transaction.Creatable = {
-			counterpart: { type: "paxgiro", identifier: targetPaxgiro },
+			counterpart: { type: type, identifier: targetPaxgiro },
 			currency: "USD",
 			amount: 1,
-			description: "upcheck internal transaction",
+			description: "upcheck paxgiro transaction",
 		}
 		const paxgiro = await client?.transactions.create(sourcePaxgiro, transaction)
 		expect(
